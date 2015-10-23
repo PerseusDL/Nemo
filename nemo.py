b112732cfb0e29a13865f42bdac34c8cbe5148f8
Thibault Clerice <leponteineptique@gmail.com>
2015-10-23 16:17:11 +0200

    With more config

--- nemo.py
+++ nemo.py
@@ -16,9 +16,27 @@ nemo = Nemo(
     base_url="",
     name="nemo",
     # In our case, we have an inventory named "nemo"
-    inventory="nemo"#,
+    inventory="nemo",
+    css=[
+            # USE Own CSS
+            "static/tei.pb.min.css",
+            "static/ciham.css"
+    ],
+    js=[
+            # use own js file to load a script to go from normalized edition to diplomatic one.
+            "static/ciham.js"
+    ],
+    transform={
+        "urn:cts:froLit:jns915.jns1856.ciham-fro1" : "static/ciham.xslt"
+    }
     # We give thee ap object
     #app=app
+    chunker={
+        # The default chunker takes care of book, poem, lines
+        # but it would be cool to have 30 lines group for Nemo
+        "urn:cts:froLit:jns915.jns1856.ciham-fro1": lambda text, cb: [(reff.split(":")[-1], reff.split(":")[-1]) for reff in cb(1)],
+        "default": Nemo.scheme_chunker  # lambda text, cb: Nemo.line_grouper(text, cb, 50)
+    }
 )
 nemo.init_app(app)
 # We register its routes
