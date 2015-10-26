688ca4cfe3e4573c463dd0353d0d8b6546908d76
Bridget Almas <balmas@gmail.com>
2015-10-26 16:18:56 -0400

    missing comma

--- nemo.py
+++ nemo.py
@@ -36,9 +36,9 @@ nemo = Nemo(
         # but it would be cool to have 30 lines group for Nemo
         "urn:cts:froLit:jns915.jns1856.ciham-fro1": lambda text, cb: [(reff.split(":")[-1], reff.split(":")[-1]) for reff in cb(1)],
         "default": Nemo.scheme_chunker  # lambda text, cb: Nemo.line_grouper(text, cb, 50),
-    }
-    cache = "nemocache",
-    expire = 10
+    },
+    cache="nemocache",
+    expire=10
 )
 nemo.init_app(app)
 # We register its routes
