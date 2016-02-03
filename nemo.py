2aeaec354aa9e9d99c990731459d8ff406ef4ee5
maryam-foradi <maryam.foradi@gmail.com>
2016-02-03 13:42:13 +0330

    (stoa0276.stoa006.opp-lat) Updated Nemo.py
    Show 30 lines of the poem

--- nemo.py
+++ nemo.py
@@ -37,6 +37,7 @@ nemo = Nemo(
         # but it would be cool to have 30 lines group for Nemo
         "urn:cts:latinLit:phi0959.phi005.perseus-lat2": lambda version, callback: Nemo.line_chunker(version, callback, lines=30),
         "urn:cts:froLit:jns915.jns1856.ciham-fro1": lambda text, cb: [(reff.split(":")[-1], reff.split(":")[-1]) for reff in cb(1)],
+        "urn:cts:latinLit:stoa0276.stoa006.opp-lat1": lambda version, callback: Nemo.line_chunker(version, callback, lines=30),
         "default": Nemo.scheme_chunker  # lambda text, cb: Nemo.line_grouper(text, cb, 50),
     }, 
 
